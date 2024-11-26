90100e7941d89e9ef3da809b81e2ec076d4ce3dd
Etienne Dumai <etienne.dumai@gmail.com>
2024-11-26 10:11:39 +0100

    debut du td/tp observateur

---
+++ TP/TP7/src/ObservateurMeteo.java
@@ -0,0 +1,57 @@
+public class ObservateurMeteo {
+    private String libelleObservateur;
+    private ObservableMeteo observable;
+    private String dateHeure;
+    private double temperature;
+    private double pression;
+
+    public ObservateurMeteo(String libelleObservateur) {
+        this.libelleObservateur = libelleObservateur;
+    }
+
+    public void setObservable(ObservableMeteo observable) {
+        this.observable = observable;
+    }
+
+    public String getLibelleObservateur() {
+        return libelleObservateur;
+    }
+
+    public void setLibelleObservateur(String libelleObservateur) {
+        this.libelleObservateur = libelleObservateur;
+    }
+
+    public String getDateHeure() {
+        return dateHeure;
+    }
+
+    public void setDateHeure(String dateHeure) {
+        this.dateHeure = dateHeure;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getPression() {
+        return pression;
+    }
+
+    public void setPression(double pression) {
+        this.pression = pression;
+    }
+
+    @Override
+    public String toString() {
+        return "ObservateurMeteo{" +
+                "libelleObservateur='" + libelleObservateur + '\'' +
+                ", dateHeure='" + dateHeure + '\'' +
+                ", temperature=" + temperature +
+                ", pression=" + pression +
+                '}';
+    }
+}
